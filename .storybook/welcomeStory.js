be6ceae8df93beda7dbcacd15c6b979c93e61684
paher <paher@expedia.com>
2018-10-22 23:08:04 -0700

    jsx addon

---
+++ .storybook/welcomeStory.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Welcome Page', module)
+    .add('this is the welcome page', () => (<h1>Welcome to storybook guys!</h1>));
