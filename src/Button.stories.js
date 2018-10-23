1af2b97683ccf6af1097f8b3c037155e29a787a4
paher <paher@expedia.com>
2018-10-22 23:40:00 -0700

    adding info and knob addon

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from './Button';
+import { withInfo } from '@storybook/addon-info';
+import { color } from '@storybook/addon-knobs/react';
 
 storiesOf('Button', module)
-    .addWithJSX('with background', () => <Button bg="palegoldenrod">Hello world</Button>)
-    .addWithJSX('with background 2', () => <Button bg="green">Hello world2</Button>);
+    .addWithJSX('with background', withInfo(`description of the component`) (() => <Button bg="palegoldenrod">Hello world</Button>))
+    .addWithJSX('with background 2', withInfo(`description of the component`) (() => <Button bg={color('bg', 'green', 'group1')}>Hello world2</Button>));
